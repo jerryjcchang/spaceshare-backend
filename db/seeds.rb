8bf38a280b26412b86d4753f0cd70d99f86dabba
github username <github email address>
2019-10-29 18:10:04 -0400

    removed byebug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 require 'bcrypt'
 require 'geocoder'
 require 'rest-client'
-require 'byebug'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
