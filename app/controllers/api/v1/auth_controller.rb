91a0b424e9d39a1b72a8e56af2e746fca9a7e8db
github username <github email address>
2019-10-31 15:20:18 -0400

    sanitized login

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::AuthController < ApplicationController
   def create
-    @user = User.find_by(email: params[:email])
+    @user = User.find_by(email: params[:email].downcase)
     if @user && @user.authenticate(params[:password])
       render json: {
         message: "correct email and password",
         error: false,
         user_info: {user: @user,
                     bookings: @user.booked_spaces,
-                    spaces: @user.hosted_spaces 
+                    spaces: @user.hosted_spaces
                    },
         token: encode({user_id: @user.id}),
         status: :accepted
